1cc81596d39ba29ac30c92445431c7ffa0a55836
Manpreet <mp_manpreet@hotmail.com>
2023-07-25 04:23:18 +0000

    boiler plate

--- 1_queries/all_reservations.sql
+++ 1_queries/all_reservations.sql
@@ -1,8 +1,8 @@
-SELECT reservations.id, title, start_date, cost_per_night, AVG(rating) AS average_rating
+SELECT reservations.id, title, start_dates, cost_per_night, AVG(rating) AS average_rating
 FROM reservations
 JOIN properties ON properties.id = reservations.property_id
 JOIN property_reviews ON properties.id = property_reviews.property_id
 WHERE reservations.guest_id = 1
 GROUP BY reservations.id, properties.title, cost_per_night
-ORDER BY start_date
+ORDER BY start_dates
 LIMIT 10;
